026fe8cfbf11ec333a4475f9118a9b6634ffae4f
Rochelle Lewis <rlewis37@gmail.com>
2019-02-12 15:01:30 -0700

    add base files from current tutorial progress

---
+++ src/app.js
@@ -0,0 +1,46 @@
+
+//these components are akin to classes. This is a function component
+const Pet = (props) => {
+	return React.createElement("div", {}, [
+		React.createElement("h1", {}, props.name),
+		React.createElement("h2", {}, props.animal),
+		React.createElement("h2", {}, props.breed)
+	]);
+}
+
+// this is a class component
+class App extends React.Component {
+	handleTitleClick() {
+		alert("you clicked the title!");
+	}
+
+	render() {
+		//these are stamping onto the page
+		return React.createElement("div",  {id: "my-id"},
+			React.createElement("h1", { onClick: this.handleTitleClick }, "Adopt Me!"),
+			React.createElement(Pet, {
+				name: "Luna",
+				animal: "Dog",
+				breed: "Havanese"
+			}),
+			React.createElement(Pet, {
+				name: "Pepper",
+				animal: "Bird",
+				breed: "Cockatiel"
+			}),
+			React.createElement(Pet, {
+				name: "Doink",
+				animal: "Cat",
+				breed: "Mixed"
+			})
+		)
+	}
+}
+
+// first parent component
+/*const App = () => {
+
+}*/
+
+//render code
+ReactDOM.render(React.createElement(App), document.getElementById('root'))
