2387e8710a931fa85d5e907d7d0b1849e5d4a364
Rochelle Lewis <rlewis37@gmail.com>
2019-02-12 20:40:05 -0700

    add jsx

--- src/app.js
+++ src/app.js
@@ -10,29 +10,39 @@ class App extends React.Component {
 
   render() {
     //these are stamping onto the page
-    return React.createElement(
-      "div",
-      { id: "my-id" },
-      React.createElement(
-        "h1",
-        { onClick: this.handleTitleClick },
-        "Adopt Me!"
-      ),
-      React.createElement(Pet, {
-        name: "Luna",
-        animal: "Dog",
-        breed: "Havanese"
-      }),
-      React.createElement(Pet, {
-        name: "Pepper",
-        animal: "Bird",
-        breed: "Cockatiel"
-      }),
-      React.createElement(Pet, {
-        name: "Doink",
-        animal: "Cat",
-        breed: "Mixed"
-      })
+    // return React.createElement(
+    //   "div",
+    //   {},
+    //   React.createElement(
+    //     "h1",
+    //     { onClick: this.handleTitleClick },
+    //     "Adopt Me!"
+    //   ),
+    //   React.createElement(Pet, {
+    //     name: "Luna",
+    //     animal: "Dog",
+    //     breed: "Havanese"
+    //   }),
+    //   React.createElement(Pet, {
+    //     name: "Pepper",
+    //     animal: "Bird",
+    //     breed: "Cockatiel"
+    //   }),
+    //   React.createElement(Pet, {
+    //     name: "Doink",
+    //     animal: "Cat",
+    //     breed: "Mixed"
+    //   })
+    // );
+
+    //JSX version of above
+    return (
+      <div>
+        <h1 onClick={this.handleTitleClick}>Adopt Me!</h1>
+        <Pet name="Luna" animal="dog" breed="Havanese" />
+        <Pet name="Pepper" animal="bird" breed="Chicken" />
+        <Pet name="Doink" animal="cat" breed="Mixed" />
+      </div>
     );
   }
 }
--- src/pet.js
+++ src/pet.js
@@ -2,11 +2,20 @@
 import React from "react";
 
 export const Pet = props => {
-	return React.createElement("div", {}, [
-		React.createElement("h1", {}, props.name),
-		React.createElement("h2", {}, props.animal),
-		React.createElement("h2", {}, props.breed)
-	]);
+	// return React.createElement("div", {}, [
+	// 	React.createElement("h1", {}, props.name),
+	// 	React.createElement("h2", {}, props.animal),
+	// 	React.createElement("h2", {}, props.breed)
+	// ]);
+
+	// JSX version of the above
+	return (
+		<div className="my-class">
+			<h1>{ props.name }</h1>
+			<h2>{ props.animal }</h2>
+			<h2>{ props.breed }</h2>
+		</div>
+	);
 };
 
 export default Pet;
