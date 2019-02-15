0aa47f6aa4e514f837b34cccb242d5238a80b7e4
Rochelle Lewis <rlewis37@gmail.com>
2019-02-15 13:18:22 -0700

    display pet photo + location

--- src/Pet.js
+++ src/Pet.js
@@ -1,15 +1,29 @@
 import React from "react";
 
-export const Pet = props => {
+class Pet extends React.Component {
+	render() {
+		// destructuring here - pulling things out of the object. For clarity?
+		const { name, animal, breed, media, location } = this.props;
 
-	// JSX version of React.createElement
-	return (
-		<div className="my-class">
-			<h1>{ props.name }</h1>
-			<h2>{ props.animal }</h2>
-			<h2>{ props.breed }</h2>
-		</div>
-	);
-};
+		let photos = [];
+		if(media && media.photos && media.photos.photo) {
+			photos = media.photos.photo.filter(photo => photo["@size"] === 'pn');
+		} else {
+			photos[0] = "url missing";
+		}
+
+		return (
+			<div className="pet">
+				<div className="image-container">
+					<img src={photos[0].value} alt={name}/>
+				</div>
+				<div className="info">
+					<h1>{name}</h1>
+					<h2>{`${animal} — ${breed} — ${location}`}</h2>
+				</div>
+			</div>
+		);
+	}
+}
 
 export default Pet;
