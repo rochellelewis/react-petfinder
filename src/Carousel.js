a9d5657f23b5a42b96dfb16903b33e7d0dc970c3
Rochelle Lewis <rlewis37@gmail.com>
2019-02-15 16:37:37 -0700

    add interactivity to carousel component

--- src/Carousel.js
+++ src/Carousel.js
@@ -6,7 +6,7 @@ class Carousel extends React.Component {
 		active: 0
 	};
 
-	// this is a builtin React function
+	// this function is a React builtin
 	static getDerivedStateFromProps({media}) {
 		let photos = [];
 		if (media && media.photos && media.photos.photo) {
@@ -14,6 +14,18 @@ class Carousel extends React.Component {
 		}
 
 		return {photos};
+	};
+
+	/*
+	* NOTE: functions vs arrow functions have subtle difference semantics in re: to binding this (keyword)
+	* handleIndexClick will only work here as an arrow fn - will not create a new scope!
+	* arrow fn does not create a new context when it's called, reverts to the lexical scope it's in
+	**/
+	handleIndexClick = event => {
+		this.setState({
+			// cast this string into a number - coercion
+			active: +event.target.dataset.index
+		})
 	};
 
 	render() {
@@ -24,8 +36,11 @@ class Carousel extends React.Component {
 				<img src={photos[active].value} alt="primary animal"/>
 				<div className="carousel-smaller">
 					{photos.map((photo, index) => (
+						/* eslint-disable-next-line */
 						<img
+							onClick={this.handleIndexClick}
 							key={photo.value}
+							data-index={index}
 							src={photo.value}
 							className={index === active ? "active" : ""}
 							alt="animal thumbnail"
