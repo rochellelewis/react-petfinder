2ff6dcaf4e4428c8e2b7f14fa0e46d3ee84064f8
Rochelle Lewis <rlewis37@gmail.com>
2019-02-15 12:43:34 -0700

    load info from petfinder api

--- src/App.js
+++ src/App.js
@@ -1,19 +1,81 @@
 import React from "react";
 import {render} from 'react-dom';
+import pf from "petfinder-client";
 import Pet from "./Pet";
+
+const petfinder = pf({
+   key: process.env.API_KEY,
+   secret: process.env.API_SECRET
+});
 
 // this is a class component
 class App extends React.Component {
 
-  render() {
+   // calling the parent component's constructor - passed in props from Pet.
+   // this overrides the default constructor from the React.Component
+   constructor(props) {
+      super(props);
+
+      //define initial state
+      this.state = {
+         pets: []
+      }
+   }
+
+   // this is a lifecycle method. renders to the DOM first,
+   // then calls this method after the DOM mounted. called once per component
+   componentDidMount() {
+
+      petfinder.pet.find({output: "full", location: "Albuquerque, NM"})
+         .then(data => {
+            let pets;
+
+            // check if data is there
+            if(data.petfinder.pets && data.petfinder.pets.pet) {
+               if (Array.isArray(data.petfinder.pets.pet)) {
+                  pets = data.petfinder.pets.pet;
+               } else {
+                  pets = [data.petfinder.pets.pet];
+               }
+            } else {
+               pets = [];
+            }
+
+            // store pets into the state declared on line 21
+            this.setState({
+               pets
+            });
+         });
+   }
+
+   render() {
 
     //JSX version React.createElement
     return (
       <div>
         <h1>Adopt Me!</h1>
-        <Pet name="Luna" animal="dog" breed="Havanese" />
-        <Pet name="Pepper" animal="bird" breed="Chicken" />
-        <Pet name="Doink" animal="cat" breed="Mixed" />
+        <div>
+           {/* transforming the pets array into pets components */}
+           {this.state.pets.map(pet => {
+
+              // account for multiple breed values and join if necs.
+              let breed;
+              if (Array.isArray(pet.breeds.breed)) {
+                 breed = pet.breeds.breed.join(', ');
+              } else {
+                 breed = pet.breeds.breed;
+              }
+
+              // output data on screen
+              return (
+                 <Pet
+                    animal={pet.animal}
+                    name={pet.name}
+                    breed={breed}
+                 />
+              );
+           })}
+        </div>
       </div>
     );
   }
