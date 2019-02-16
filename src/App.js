f01900c5bba73c7a706f7458c3e8bdd7f9ead9b9
Rochelle Lewis <rlewis37@gmail.com>
2019-02-16 14:29:27 -0700

    move event handlers from SearchParams to App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,71 @@
 import React from "react";
 import {render} from 'react-dom';
-import {Router} from "@reach/router";
+import {Router, Link} from "@reach/router";
+import pf from "petfinder-client";
+import {Provider} from "./SearchContext";
 import Results from './Results';
 import Details from "./Details";
-import {Link} from "@reach/router";
 import SearchParams from "./SearchParams";
 
 class App extends React.Component {
+	// this constructor is here for context
+	constructor (props) {
+		super(props);
+
+		this.state = {
+			location: "Albuquerque, NM",
+			animal: "",
+			breed: "",
+			breeds: [],
+			handleAnimalChange: this.handleAnimalChange,
+			handleBreedChange: this.handleBreedChange,
+			handleLocationChange: this.handleLocationChange,
+			getBreeds: this.getBreeds
+		};
+	}
+
+	// handles input change event - data coming in from search field
+	handleLocationChange = event => {
+		this.setState({
+			location: event.target.value
+		})
+	};
+
+	// def use arrow fns w/ event handling! For optmization! (bind is too expensive!)
+	handleAnimalChange = event => {
+		this.setState({
+			animal: event.target.value,
+			breed: ""
+		}, this.getBreeds);
+	};
+
+	handleBreedChange = event => {
+		this.setState({
+			breed: event.target.value
+		});
+	};
+
+	getBreeds() {
+		if(this.state.animal) {
+			petfinder.breed.list({animal: this.state.animal})
+				.then(data => {
+					if(
+						data.petfinder &&
+						data.petfinder.breeds &&
+						Array.isArray(data.petfinder.breeds.breed)
+					) {
+						this.setState({
+							breeds: data.petfinder.breeds.breed
+						})
+					} else {
+						this.setState({breeds: []});
+					}
+				})
+		} else {
+			this.setState({breeds: []});
+		}
+	}
+
 	render() {
 		return (
 			<div>
--- src/SearchParams.js
+++ src/SearchParams.js
@@ -7,54 +7,6 @@ const petfinder = pf({
 });
 
 class SearchParams extends React.Component {
-	state = {
-		location: "Albuquerque, NM",
-		animal: "",
-		breed: "",
-		breeds: []
-	};
-
-	// handles input change event - data coming in from search field
-	handleLocationChange = event => {
-		this.setState({
-			location: event.target.value
-		})
-	};
-
-	// def use arrow fns w/ event handling! For optmization! (bind is too expensive!)
-	handleAnimalChange = event => {
-		this.setState({
-			animal: event.target.value,
-			breed: ""
-		}, this.getBreeds);
-	};
-
-	handleBreedChange = event => {
-		this.setState({
-			breed: event.target.value
-		});
-	};
-
-	getBreeds() {
-		if(this.state.animal) {
-			petfinder.breed.list({animal: this.state.animal})
-				.then(data => {
-					if(
-						data.petfinder &&
-						data.petfinder.breeds &&
-						Array.isArray(data.petfinder.breeds.breed)
-					) {
-						this.setState({
-							breeds: data.petfinder.breeds.breed
-						})
-					} else {
-						this.setState({breeds: []});
-					}
-				})
-		} else {
-			this.setState({breeds: []});
-		}
-	}
 
 	render() {
 		return (
