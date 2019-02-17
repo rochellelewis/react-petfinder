bd0f2eb31ca43ed48c536c414f631efd6b9e7716
Rochelle Lewis <rlewis37@gmail.com>
2019-02-16 17:26:57 -0700

    create modal markup for details view

--- src/Details.js
+++ src/Details.js
@@ -2,6 +2,7 @@ import React from "react";
 import pf from "petfinder-client";
 import { navigate } from "@reach/router";
 import Carousel from "./Carousel";
+import Modal from "./Modal";
 
 const petfinder = pf({
 	key: process.env.API_KEY,
@@ -11,7 +12,12 @@ const petfinder = pf({
 class Details extends React.Component {
 	// we removed the constructor since babel will allow it
 	state = {
-		loading: true
+		loading: true,
+		showModal: true
+	};
+
+	toggleModal = () => {
+		this.setState({showModal: !this.state.showModal});
 	};
 
 	componentDidMount() {
@@ -50,7 +56,22 @@ class Details extends React.Component {
 			return <h1>Loading...</h1>;
 		}
 
-		const { name, animal, breed, location, description, media } = this.state;
+		const { name, animal, breed, location, description, media, showModal } = this.state;
+
+		/* setup modal markup */
+		let modal;
+		if (showModal) {
+			modal =
+				<Modal>
+					<h1>Would you like to adopt {name}?</h1>
+					<div className="buttons">
+						<button onClick={this.toggleModal}>Yes</button>
+						<button onClick={this.toggleModal}>Hell Yes! :D</button>
+					</div>
+				</Modal>;
+		} else {
+			modal = null;
+		}
 
 		return (
 			<div className="details">
@@ -60,7 +81,9 @@ class Details extends React.Component {
 					<h2>
 						{animal} - {breed} - {location}
 					</h2>
+					<button onClick={this.toggleModal}>Adopt {name}</button>
 					<p>{description}</p>
+					{modal}
 				</div>
 			</div>
 		);
