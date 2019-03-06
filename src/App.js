728052928f90bb685668ebddc265ad108f1bbd34
Rochelle Lewis <rlewis37@gmail.com>
2019-03-05 22:03:03 -0700

    remove all code splits, save Details component for example

--- src/App.js
+++ src/App.js
@@ -7,24 +7,9 @@ import NavBar from "./NavBar";
 import {Provider} from "react-redux";
 import store from "./store";
 
-//parcel will dynamically separate Details in to a separate bundle
+//parcel will dynamically separate Details in to a separate bundle. just a test.
 const LoadableDetails = Loadable({
 	loader: () => import("./Details"),
-	loading() {
-		return <h1>loading split component...</h1>
-	}
-});
-
-//you can make these loadables into a higher order component if u wanted to....
-const LoadableResults = Loadable({
-	loader: () => import("./Results"),
-	loading() {
-		return <h1>loading split component...</h1>
-	}
-});
-
-const LoadableSearchParams = Loadable({
-	loader: () => import("./SearchParams"),
 	loading() {
 		return <h1>loading split component...</h1>
 	}
@@ -40,8 +25,8 @@ class App extends React.Component {
 					{/* Anything inside of Provider can access the context using the exported Consumer */}
 					<Router>
 						<LoadableResults path="/" />
-						<LoadableDetails path="/details/:id" />
-						<LoadableSearchParams path="/search-params" />
+						<Details path="/details/:id" />
+						<SearchParams path="/search-params" />
 					</Router>
 				</Provider>
 			</div>
