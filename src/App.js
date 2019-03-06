c367e0dc9e722c9557cf8ff44faee829fb7589b8
Rochelle Lewis <rlewis37@gmail.com>
2019-03-05 21:58:02 -0700

    play around with code splitting some components here...

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React from "react";
 import { render } from "react-dom";
 import { Router } from "@reach/router";
 import Loadable from 'react-loadable';
-import Results from "./Results";
-import SearchParams from "./SearchParams";
 import NavBar from "./NavBar";
 
 import {Provider} from "react-redux";
@@ -12,6 +10,21 @@ import store from "./store";
 //parcel will dynamically separate Details in to a separate bundle
 const LoadableDetails = Loadable({
 	loader: () => import("./Details"),
+	loading() {
+		return <h1>loading split component...</h1>
+	}
+});
+
+//you can make these loadables into a higher order component if u wanted to....
+const LoadableResults = Loadable({
+	loader: () => import("./Results"),
+	loading() {
+		return <h1>loading split component...</h1>
+	}
+});
+
+const LoadableSearchParams = Loadable({
+	loader: () => import("./SearchParams"),
 	loading() {
 		return <h1>loading split component...</h1>
 	}
@@ -26,9 +39,9 @@ class App extends React.Component {
 				<Provider store={store}>
 					{/* Anything inside of Provider can access the context using the exported Consumer */}
 					<Router>
-						<Results path="/" />
+						<LoadableResults path="/" />
 						<LoadableDetails path="/details/:id" />
-						<SearchParams path="/search-params" />
+						<LoadableSearchParams path="/search-params" />
 					</Router>
 				</Provider>
 			</div>
